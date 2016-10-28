5b5eb636dddc867fbe5d393d3ca09a5e6871d4dd
a930334 <josecarlos.santosruiz@gmail.com>
2016-10-28 22:08:06 +0200

    Modificaciones varias

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -61,6 +61,9 @@ function onDeviceReady() {
         var elementos;
         var audio_en_rep="0";
         var posicion=0;
+        var reproductor;
+        var reproduciendo=0;
+        var descargando = false;
 
         $.getJSON( "https://api.spreaker.com/v2/shows/1060718/episodes?limit=15", function( data ) {
             var items = [];
@@ -71,13 +74,27 @@ function onDeviceReady() {
                 cadena = "<li class=\"episodios\" id=\"" + i++ + "\">" + val.title.substring(0, 50) + "</li>";
                 $("#listado").append(cadena);
             }); // fin de forEach
+            // Esto hay que sacarlo a una función externa, que lo tengo dos veces igual...
             $("#imagen").html("<img align=\"center\" src="+elementos[0].image_url.replace("\/","/")+">");
+            $("#imagen2").html("<img align=center src="+elementos[0].image_url.replace("\/","/")+" >");
+            episodio_id = elementos[0].episode_id;
+            audio_en_rep = "https://api.spreaker.com/listen/episode/"+episodio_id+"/http";
+            reproductor = new Media(encodeURI(audio_en_rep), function(){console.log("comenzando reproduccion")},
+                                                             function(err){console.log("Error en reproduccion" + err.code)},
+                                                             function(msg){reproduciendo = msg; console.log("Estado de la reproduccion" + reproduciendo)});
         }); // fin de getJSON
 
         $("#listado").delegate('.episodios','click',function(){
             posicion = this.id;
             $("#imagen").html("<img align=center src="+elementos[posicion].image_url.replace("\/","/")+" >");
             $("#imagen2").html("<img align=center src="+elementos[posicion].image_url.replace("\/","/")+" >");
+            episodio_id = elementos[posicion].episode_id;
+            audio_en_rep = "https://api.spreaker.com/listen/episode/"+episodio_id+"/http";
+            reproductor = new Media(encodeURI(audio_en_rep), function(){console.log("comenzando reproduccion")},
+                                                             function(err){console.log("Error en reproduccion" + err.code)},
+                                                             function(msg){reproduciendo = msg; console.log("Estado de la reproduccion" + reproduciendo)});
+             console.log("Estado de la reproduccion 1a" + reproduciendo)
+            $.mobile.changePage($("#capitulo"), { transition: "slideup"} );
         }); // final click episodio
 
         $("#descarga").click(function(){
@@ -85,13 +102,15 @@ function onDeviceReady() {
             var fileTransfer = new FileTransfer();
             var audio_en_rep = "https://api.spreaker.com/listen/episode/"+episodio_id+"/http";
             var uri = encodeURI(audio_en_rep);
-            var fileURL =  "///storage/sdcard/Podcasts/"+episodio_id+".mp3"; // emulador
+            //var fileURL =  "///storage/sdcard/Podcasts/"+episodio_id+".mp3"; // emulador
             //var fileURL =  "///storage/emulated/0/Podcasts/"+episodio_id+".mp3"; // móvil
+            var fileURL =  cordova.file.dataDirectory + episodio_id + ".mp3";
 
             fileTransfer.onprogress = function(progressEvent) {
                 if (progressEvent.lengthComputable) {
             	    var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
             		$("#pctDesc").html("<p>Descargado " + perc + "%...<p>");
+            		$("ft-prog").value = perc;
             	} else {
             	    if($("#pctDesc").html() == "") {
             		    $("#pctDesc").html("Descargando...");
@@ -101,24 +120,47 @@ function onDeviceReady() {
             	}
             }; // fin onprogress
 
-            console.log("Comenzando la descarga del fichero "+ audio_en_rep);
-            $("#pctDesc").html("");
-            fileTransfer.download(
-                uri, fileURL, function(entry) {
-                console.log("download complete: " + entry.toURL());
-            },
-            function(error) {
-                console.log("download error source " + error.source);
-                console.log("download error target " + error.target);
-                console.log("download error code" + error.code);
-            },
-            false, {
-                headers: {
-                    "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
-                }
-            })
-            alert("Descarga completa");
+            if (!descargando){
+                descargando = true;
+            	$("#descarga").html("Cancelar descarga");
+                console.log("Comenzando la descarga del fichero "+ audio_en_rep);
+                $("#pctDesc").html("");
+                fileTransfer.download(
+                    uri, fileURL, function(entry) {
+                    console.log("download complete: " + entry.toURL());
+                },
+                function(error) {
+                    console.log("download error source " + error.source);
+                    console.log("download error target " + error.target);
+                    console.log("download error code" + error.code);
+                },
+                false, {
+                    headers: {
+                        "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                    }
+                })
+            }
+            else {
+                descargando = false;
+            	$("#descarga").html("Descargar");
+                $("#pctDesc").html("");
+                fileTransfer.abort();
+            }
         }); //fin ("#descarga").click
+
+
+        $("#buttonplay").click(function(){
+            console.log ("Estado rep "  + reproduciendo);
+            if (reproduciendo == Media.MEDIA_RUNNING ) {
+                reproductor.pause();
+                $("#buttonplay").html("Reproducir") ;
+                reproduciendo = false;
+            } else {
+                reproductor.play();
+                $("#buttonplay").html("Pausa") ;
+                reproduciendo = true;
+            }
+        });
     }
 
 /****************************** directorio ************************************/
